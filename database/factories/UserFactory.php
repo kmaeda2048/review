cf2d93a3771e2e7624f6e06947f147f651cb20fe
First-name Family-name <username@example.com>
2020-07-05 19:12:18 +0900

    Edit UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'image_url' => 'https://placehold.jp/150x150.png',
         'remember_token' => Str::random(10),
     ];
 });
